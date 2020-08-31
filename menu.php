e93265fa3263842541caeca22f858430b9669ec7
altairsot <43548450+altairsot@users.noreply.github.com>
2020-08-31 14:22:23 -0300

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,5 +1,5 @@
 <?php
-    echo '<a class="active" href="https://altairsot.github.io/index.php">Página Principal</a>
-	<a href="https://altairsot.github.io/ensino.php">Ensino</a>
-	<a href="https://altairsot.github.io/pesquisa.php">Pesquisa</a>';
+    echo '<a class="active" href="https://altairsot.github.io/index.html">Página Principal</a>
+	<a href="https://altairsot.github.io/ensino.html">Ensino</a>
+	<a href="https://altairsot.github.io/pesquisa.html">Pesquisa</a>';
 ?>
