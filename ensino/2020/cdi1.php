a63d15b22259fca1874ebfc8f705899b4a22a44d
altairsot <43548450+altairsot@users.noreply.github.com>
2020-08-31 14:10:29 -0300

    Add files via upload

---
+++ ensino/2020/cdi1.php
@@ -0,0 +1,80 @@
+<html>
+<head> <!-- CABEÇALHO -->
+	<meta charset="utf-8" />
+	<meta name="author" content="Altair" >
+	<meta content="text/html; charset=UTF-8">
+	<title>. : Altair Santos de Oliveira-Tosti : .</title>
+	<meta name="description" content="Homepage of Altair Santos de Oliveira Tosti"/>
+	<meta name="keywords" content="Altair Santos de Oliveira Tosti, Altair Tosti"/>
+	<meta name="viewport"acontent="width=device-width, initial-scale=1">	
+	<link href="../../css/estilo.css" rel="stylesheet">
+	<!--Thanks to Yuri Santos-->
+	<link type="text/css" rel="stylesheet" media="only screen and (max-device-width: 480px)" href="../../css/menor.css" />
+	<link type="text/css" rel="stylesheet" media="only screen and (min-device-width: 481px)" href="../../css/maior.css" />
+</head>
+
+<!DOCTYPE html>
+	<title>. . : : Altair Santos de Oliveira-Tosti : : . .</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!--<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">-->
+	<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<style>
+		html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
+	</style>
+	<body class="w3-light-grey">
+<!-- Menu de navegação-->
+<div class="topnav">
+    <?php include 'menu.php';?>	
+</div>
+<!-- Page Container -->
+<div class="w3-content w3-margin-top" style="max-width:1400px;">
+	<!-- The Grid -->
+	<div class="w3-row-padding">
+		<div class="w3-container w3-card w3-white w3">
+			<h3 class="w3-padding-16">Cálculo Diferencial e Integral 1 - 2020</h3>
+				<div class="w3-container w3-card w3-white w3-margin-bottom">
+        				<h5 class="w3-opacity w3-center"><b>Ementa</b></h5>
+						<p>Limite. Funções Contínuas. Derivadas. Regras de Derivação. Aplicações de Derivada. Integrais Indefinidas. Integrais Definidas. Técnicas de Integração. Integrais Impróprias. Aplicações de Integral. Desenvolvimento e Orientação de atividades de Prática como Componente Curricular.</p>
+					<h5 class="w3-opacity w3-center"><b>Horário de Atendimento</b></h5>
+						<p>Terça-feira: 15h00 - 18h00.</p>
+						<p>Quarta-feira: 21h00 - 21h50 (Horário assíncrono).</p>
+					<h5 class="w3-opacity w3-center"><b>Formas e Critérios de Avaliação</b></h5>
+						<p>A avaliação da aprendizagem se dará de forma contínua durante todo o período letivo.</p>
+    						<p>Serão realizadas 04 (quatro) provas escritas, cujas notas serão denotadas por P1, P2, P3 e P4. Todas as provas terão valores mínimo 0,0 (zero) e máximo 10,0 (dez). As provas P1 e P2 serão aplicadas no primeiro semestre letivo, enquanto que as provas P3 e P4 serão aplicadas no segundo semestre letivo.</p>
+    						<p>Atividades assíncronas ou na plataforma AVA, cujas datas ficam a cargo do docente. A média simples das notas dessas atividades será denotada por MA.</p>
+    						<p>Trabalhos individuais ou em grupo. A média simples dos trabalhos será denotada por MT.</p>
+    						<p>A média final (MF) do discente será calculada da seguinte maneira: (2xMT + 2xMA + 2xP1 + 3xP2 + 3xP3 + 4xP4) / 16.</p>
+					<h5 class="w3-opacity w3-center"><b>Referências</b></h5>
+						<p>
+							<b>Referência Básica</b><br>
+							LEITHOLD, L. O Cálculo com Geometria Analítica. 3. ed. São Paulo: Harbra, 1994;<br>
+							GUIDORIZZI, H. L. Um Curso de Cálculo. Vol 1. 5. ed. Rio de Janeiro: LTC, 2003;<br>
+							STEWART, J. Cálculo Volume 1. 7. ed. São Paulo: CENGAGE Learning, 2013;<br>
+							<b>Referência Complementar</b><br>
+							APOSTOL, T. M. Calculus, Volume 1, One-Variable Calculus, with an Introduction to Linear Algebra, Wiley, 1967;<br>
+							ÁVILA, G. Cálculo das funções de uma Variável. Vol. 1 e 2. 7 ed. Rio de Janeiro: LTC, 2007;<br>
+							BANNER, A. The Calculus Lifesaver: All the Tools You Need to Excel at Calculus. Princeton University Press, 2007;<br>
+							COURANT, R. Differential and Integral Calculus, Vol. I, translated by McShane, E. J., 2ª ed., New York: Interscience, 1937;<br>
+							LANG, S. Cálculo - Vol I. Serge Lang. Ed. Globo, 1973.LIPSCHUTZ, S. O Cálculo com Geometria Analítica. Vol. I. Ed. Harper & Row do Brasil, 1990;<br>
+							PISKUNOV, N. S. Cálculo diferencial e integral. 11. ed. Porto: Lopes da Silva, 1997;<br>
+							SWOKOWSKI, E. W. Cálculo com Geometria Analítica. Tradução Alfredo Alves de Faria. São Paulo: Makron Books, 1994;<br>
+							TÁBOAS, P. Z. Cálculo em uma variável real. São Paulo: Edusp, 2008;<br>
+							THOMAS, George B. Cálculo - Volume 1. Tradução: Paulo Boschcov. São Paulo: Pearson Education do Brasil, 2002.
+						</p>
+					<h5 class="w3-opacity w3-center"><b>Notas de Aula</b></h5>
+					<p>As notas de aulas e as vídeoaulas podem ser encontradas no <i><a href="https://drive.google.com/drive/folders/1nwy4C7Phd2VzO0WF6U109V5aYwch1HiP?usp=sharing" target="_blank">Google Drive</a></i>.</p>
+					
+				</div>
+			</div>
+		</div>
+	</div>
+	 <!-- End Grid -->
+</div>
+<!-- End Page Container -->
+<footer class="w3-container w3-teal w3-center w3-margin-top">
+	<?php include 'footer.php';?> 
+</footer>
+</body>
+</html>
---
+++ ensino/2020/cdi2.php
@@ -0,0 +1,78 @@
+<html>
+<head> <!-- CABEÇALHO -->
+	<meta charset="utf-8" />
+	<meta name="author" content="Altair" >
+	<meta content="text/html; charset=UTF-8">
+	<title>. : Altair Santos de Oliveira-Tosti : .</title>
+	<meta name="description" content="Homepage of Altair Santos de Oliveira Tosti"/>
+	<meta name="keywords" content="Altair Santos de Oliveira Tosti, Altair Tosti"/>
+	<meta name="viewport"acontent="width=device-width, initial-scale=1">	
+	<link href="../../css/estilo.css" rel="stylesheet">
+	<!--Thanks to Yuri Santos-->
+	<link type="text/css" rel="stylesheet" media="only screen and (max-device-width: 480px)" href="../../css/menor.css" />
+	<link type="text/css" rel="stylesheet" media="only screen and (min-device-width: 481px)" href="../../css/maior.css" />
+</head>
+
+<!DOCTYPE html>
+	<title>. . : : Altair Santos de Oliveira-Tosti : : . .</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!--<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">-->
+	<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<style>
+		html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
+	</style>
+	<body class="w3-light-grey">
+<!-- Menu de navegação-->
+<div class="topnav">
+    <?php include 'menu.php';?>	
+</div>
+<!-- Page Container -->
+<div class="w3-content w3-margin-top" style="max-width:1400px;">
+	<!-- The Grid -->
+	<div class="w3-row-padding">
+		<div class="w3-container w3-card w3-white w3">
+			<h3 class="w3-padding-16">Cálculo Diferencial e Integral 2 - 2020</h3>
+				<div class="w3-container w3-card w3-white w3-margin-bottom">
+					<h5 class="w3-opacity w3-center"><b>Ementa e <i>Link</i> para <i>Google Meet</i></b></h5>
+						<p>Funções de várias variáveis. Derivadas parciais e derivadas direcionais. Máximos e mínimos de funções com duas ou mais variáveis. Integrais múltiplas. Integrais de linha e de superfície.</p>
+						<p>O endereço eletrônico para as aulas remotas está aqui: <i><a href="meet.google.com/hyj-gijc-ssx" target="_blank">Google Meet</a></i>.</p>
+					<h5 class="w3-opacity w3-center"><b>Horário de Atendimento</b></h5>
+						<p>Terça-feira: 15h00 - 18h00.</p>
+						<p>Terça-feira: 21h00 - 21h50 (Horário assíncrono).</p>
+					<h5 class="w3-opacity w3-center"><b>Formas e Critérios de Avaliação</b></h5>
+						<p>A avaliação da aprendizagem se dará de forma contínua durante todo o período letivo.</p>
+    						<p>Serão realizadas 04 (quatro) provas escritas, cujas notas serão denotadas por P1, P2, P3 e P4. Todas as provas terão valores mínimo 0,0 (zero) e máximo 10,0 (dez). As provas P1 e P2 serão aplicadas no primeiro semestre letivo, enquanto que as provas P3 e P4 serão aplicadas no segundo semestre letivo.</p>
+    						<p>Atividades assíncronas ou na plataforma AVA, cujas datas ficam a cargo do docente. A média simples das notas dessas atividades será denotada por MA.</p>
+    						<p>Trabalhos individuais ou em grupo. A média simples dos trabalhos será denotada por MT.</p>
+    						<p>A média final (MF) do discente será calculada da seguinte maneira: (2xMT + 2xMA + 2xP1 + 3xP2 + 3xP3 + 4xP4) / 16.</p>
+					<h5 class="w3-opacity w3-center"><b>Referências</b></h5>
+						<p>
+							<b>Referência Básica</b><br>
+							LEITHOLD, L. O Cálculo com Geometria Analítica. Vol 2. 3. ed. São Paulo: Harbra, 1994;<br>
+							GUIDORIZZI, H. L. Um Curso de Cálculo. Vol 2. 5. ed. Rio de Janeiro: LTC, 2003;<br>
+							STEWART, J. Cálculo Volume 2. 7. ed. São Paulo: CENGAGE Learning, 2013;<br>
+							<b>Referência Complementar</b><br>
+							APOSTOL, T. M. Calculus, Vol. 2: Multi-Variable Calculus and Linear Algebra with Applications to Differential Equations and Probability, 2ª ed. John Wiley, 1969;<br>
+							ÁVILA, G. Cálculo das funções de uma Variável. Vol. 1 e 2. 7 ed. Rio de Janeiro: LTC, 2007;<br>
+							COURANT, R. Differential and Integral Calculus, Vol. II, translated by McShane, E. J., New York: Interscience, 1936;<br>
+							RASSLAN, S.; TALL, D. Definitions and images for the definite integral concept. Proceedings of the 26th. Conference of the International Group for the Psychology of Mathematics Education. vol. 4, pp. 89-96, 2002;<br>
+							SIMMONS, G. F. Cálculo com geometria analítica. São Paulo: McGraw_Hill, 1987;<br>
+							SWOKOWSKI, E. W. Cálculo com geometria analítica. 2ª ed. São Paulo: McGraw Hill, 1994.
+						</p>
+					<h5 class="w3-opacity w3-center"><b>Notas de Aula</b></h5>
+					<p>As notas de aulas e as vídeoaulas podem ser encontradas no <i><a href="https://drive.google.com/drive/folders/1DSkNLsAPan0EiwZQpmmsRTkpCjcdBbLb?usp=sharing" target="_blank">Google Drive</a></i>.</p>
+					
+				</div>
+			</div>
+		</div>
+	</div>
+	 <!-- End Grid -->
+</div>
+<!-- End Page Container -->
+<footer class="w3-container w3-teal w3-center w3-margin-top">
+	<?php include 'footer.php';?> 
+</footer>
+</body>
+</html>
